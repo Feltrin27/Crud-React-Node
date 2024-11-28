fcb919d2cbace885bb021e508f76d8deeff54bcb
Erivelton Feltrin <erivelton.Feltrin@gmail.com>
2024-11-28 10:13:26 -0300

    corrigido erros do front-end

--- api/controllers/user.js
+++ api/controllers/user.js
@@ -1,10 +1,10 @@
-import {db} from "../db.js";
+import { db } from "../db.js";
 
-export const getUsers = (_, res) =>{
+export const getUsers = (_, res) => {
   const q = "SELECT * FROM usuarios";
 
   db.query(q, (err, data) => {
-    if(err) return res.json(err);
+    if (err) return res.json(err);
     return res.status(200).json(data);
   });
 };
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,32 +1,45 @@
 import GlobalStyle from './styles/global';
 import styled from "styled-components";
 import Form from "./components/Form.js";
-import {toast, ToastContainer} from "react-toastify";
+import { toast, ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 
 const Container = styled.div`
-  width:100%
-  max-width: 800px;
-  margin-top: 20px;
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  gap:10px;
+   width: 100%;
+   max-width: 800px;
+   margin-top: 20px;
+   display: flex;
+   flex-direction: column;
+   align-items: center;
+   gap: 10px;
 `;
 
 const Title = styled.h2``;
 
+// function App() {
+//   return (
+//     <>
+//       <Container>
+//         <Title>USUÁRIOS</Title>
+//         <Form />
+//       </Container>
+//       <ToastContainer autoClose={3000} position={"bottom-left"} />
+//       <GlobalStyle />
+//     </>
+//   );
+// }
 function App() {
+  console.log("Renderizando App");
   return (
-    <>
+    <div>
+      <h1>Teste básico</h1>
       <Container>
-        <Title>USUÁRIOS</Title>
-        <Form />
+      <Title>USUÁRIOS</Title>
+      <Form />
       </Container>
-      <ToastContainer autoClose={3000} position={toast.POSITION.BOTTOM_LEFT}/>
-      <GlobalStyle/>
-    </>
+      <ToastContainer autoClose={3000} position={toast.position="bottom-left"} />
+      <GlobalStyle />
+    </div >
   );
 }
-
 export default App;
